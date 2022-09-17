a07f7e53fb5fad86b236852faa7377649d1e1414
asymptoter <asymptotion@gmail.com>
2022-09-17 19:37:15 +0800

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+GIT_REPOSITORY_ROOT=$1
+
+curl https://raw.githubusercontent.com/asymptoter/gfg/master/main.go > "${GIT_REPOSITORY_ROOT}/.git/hooks/main.go"
+
+pushd ${GIT_REPOSITORY_ROOT}/.git/hooks
+
+go mod init &> /dev/null
+go mod tidy
+go build -o gfg
+rm go.mod
+rm main.go
+
+if [[ ! -f "pre-push" ]]; then
+	touch pre-push
+	echo "#!/bin/sh" >> pre-push
+	chmod +x pre-push	
+fi
+
+echo "cd "$(dirname "$0")" && ./gfg" >> pre-push
+
+popd
